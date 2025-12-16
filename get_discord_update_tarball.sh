4288f90c4cecef30796b31a0ecc111f3e809bbed
synnfynn <575507+synnfynn@users.noreply.github.com>
2025-12-16 13:31:43 -0500

    Update script.

--- get_discord_update_tarball.sh
+++ get_discord_update_tarball.sh
@@ -6,6 +6,12 @@
 
 DISCORD_URL='https://discord.com/api/download/stable?platform=linux&format=tar.gz'
 REDIRECT_URL=$(curl ${DISCORD_URL} 2> /dev/null | awk -F "https" '{print $2}' | sed -e 's/\:\/\//https\:\/\//' -e 's/\">//')
-DISCORD_TARBALL=$(echo ${REDIRECT_URL} | awk -F '/' '{print $7}')
+DISCORD_GZIPD_TARBALL=$(echo ${REDIRECT_URL} | awk -F '/' '{print $7}')
+DISCORD_TARBALL=$(echo ${DISCORD_GZIPD_TARBALL} | awk -F '.gz' '{print $1}')
 
-curl -o ${DISCORD_TARBALL} ${REDIRECT_URL}
+curl -o ${DISCORD_GZIPD_TARBALL} ${REDIRECT_URL}
+gunzip ${DISCORD_GZIPD_TARBALL}
+tar xvf ${DISCORD_TARBALL} -C ${HOME}/bin
+rm ${DISCORD_TARBALL}
+
+
